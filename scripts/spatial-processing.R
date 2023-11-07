9bbd616c8bee08cefc3953596e14acb779efd3f5
hdolinh <halina@bren.ucsb.edu>
2023-11-07 11:56:24 -0800

    filter for R5 admin units and save as new data

---
+++ scripts/spatial-processing.R
@@ -0,0 +1,20 @@
+# setup ----
+library(tidyverse)
+library(mapview)
+library(sf)
+library(here)
+library(janitor)
+
+# read in data ----
+fp_regions <- here("data/spatial/raw/S_USA.AdministrativeForest/")
+all_usfs_admin_units <- read_sf(fp_regions) %>% janitor::clean_names()
+
+# R5 au boundaries only ----
+r5_au_bounds <- all_usfs_admin_units %>% 
+  filter(region == "05")
+
+# quick map ----
+mapview(r5_au_bounds)
+
+# save R5 au boundaries ----
+sf::write_sf(r5_au_bounds, here("data/spatial/clean/r5_admin_unit_boundaries.shp"))
