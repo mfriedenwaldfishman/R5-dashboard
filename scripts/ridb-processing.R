073ab5a77ec790eda66517b45a692fd3fec5c0dd
hdolinh <halina@bren.ucsb.edu>
2023-11-07 12:01:58 -0800

    setup + read in ridb data

--- scripts/ridb-processing.R
+++ scripts/ridb-processing.R
@@ -1,3 +1,13 @@
 # setup ----
+library(vroom)
+library(here)
+library(tidyverse)
 
 # read in data ----
+# 2018 ridb data
+fp_ridb2018 <- here("data/ridb/raw/reservations2018.csv")
+raw_ridb2018 <- vroom(fp_ridb2018)
+
+# 2019 ridb data
+fp_ridb2019 <- here("data/ridb/raw/reservations2019.csv")
+raw_ridb2019 <- vroom(fp_ridb2019)
