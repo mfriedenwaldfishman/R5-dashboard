d7550bafba23eb7b486a5833709072c13c6a8c47
hdolinh <halina@bren.ucsb.edu>
2023-11-09 10:41:17 -0800

    create outline for census processing

---
+++ census-processing.R
@@ -0,0 +1,24 @@
+# setup ----
+# load libraries
+
+# read in/load data ----
+
+
+# race acs processing ----
+
+
+# education acs processing ----
+
+
+# language acs processing ----
+
+
+# median income acs processing ----
+
+
+# combine data into one data frame ----
+# use join() or rbind()
+
+
+# save data as a csv file ----
+# use write_csv()
